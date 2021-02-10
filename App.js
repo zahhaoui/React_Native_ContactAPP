32c8f5c9a5a7d5e47779efd46e37baf44fd5fa3e
zahhaoui <32593851+zahhaoui@users.noreply.github.com>
2021-02-10 18:18:12 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import Chat from './components/Chat.js'
+import SignUp from './components/Signup.js'
+import Memo from './components/Memo.js'
+import LoginView from './components/Login.js'
+import Profile from './components/Profile.js'
+import Add from './components/add.js'
+
+import Groups from './components/Groups.js'
+import UserProfileView from './components/UserProfileView.js'
+import ContactList from './components/Contact.js'
+import Menu from './components/Menu.js'
+import CallScreen from './components/CallScreen.js'
+import Calls from './components/Calls.js'
+
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+   <View style={styles.container}>
+     <Add/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
